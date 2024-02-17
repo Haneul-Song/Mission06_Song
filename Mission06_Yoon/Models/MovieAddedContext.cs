91abddaf41207bce202369d7a46fcc91fb81133c
Haneul-Song <haneulisstudying@gmail.com>
2024-02-16 23:56:48 -0700

    Add project files.

---
+++ Mission06_Yoon/Models/MovieAddedContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission06_Song.Models
+{
+    public class MovieAddedContext : DbContext
+    {
+        public MovieAddedContext(DbContextOptions<MovieAddedContext> options) : base (options) 
+        {
+        }
+
+        public DbSet<AddMovie> MoviesAdded { get; set; }
+    }
+}
+ 
